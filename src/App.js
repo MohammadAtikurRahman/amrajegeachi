94a5df269e44534ac8e7e46d31a0e148c87f6586
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2022-05-24 01:22:33 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Typography from '@mui/material/Typography';
+
+function App() {
+  return (
+    <div>
+       <Typography 
+       variant="h3"
+       color="primary"
+       align="center"
+        
+        >
+       আমরা জেগে আছি
+       </Typography>
+       
+     
+    </div>
+  );
+}
+
+export default App;
