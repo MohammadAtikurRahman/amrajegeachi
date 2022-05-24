3fff0650dd7179f2f13aa0edf5f083cf65a99633
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2022-05-24 19:54:30 +0600

    add typography

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import Typography from '@mui/material/Typography';
+import  React from 'react';
+ import Typography from '@mui/material/Typography';
+
+import Button from '@mui/material/Button';
+
 
 function App() {
   return (
+    
     <div>
-       <Typography 
+
+
+    <Typography 
        variant="h3"
        color="primary"
        align="center"
         
         >
        আমরা জেগে আছি
-       </Typography>
+       </Typography>     
+ 
+<Button variant="contained">Contained</Button>
+
+    </div>  
+    
+
        
+  
      
-    </div>
+   
   );
 }
 
